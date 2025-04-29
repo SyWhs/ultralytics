3606b23c1ec2eb418f5209fb17cdf4da972c2353
SyWhs <sywhs2020@nuaa.edu.cn>
2025-04-29 10:03:42 +0800

    优化 demo 函数，添加模型信息和结构打印，保持代码整洁

--- test.py
+++ test.py
@@ -2,19 +2,16 @@ import cv2
 from PIL import Image
 from ultralytics import YOLO
 def demo():
-    model = YOLO("ultralytics/cfg/models/v8/yolov8.yaml", verbose=True)
 
     out = model.export(format="engine", imgsz=640, dynamic=True, verbose=False, batch=8, workspace=2, half=True)
 
-    model.info()
+    model.info()  # 打印模型信息
+    model.summary()  # 打印模型结构
 
     # accepts all formats - image/dir/Path/URL/video/PIL/ndarray. 0 for webcam
     results = model.predict(source="bus.jpg", show=True)  # Display preds. Accepts all YOLO predict arguments
     for result in results:
         print(result.boxes)
-
-    # Train the model
-    results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
 
     # from PIL
     im1 = Image.open("bus.jpg")
@@ -29,4 +26,8 @@ def demo():
 
 if __name__ == "__main__":
 
-    model = YOLO("ultralytics/cfg/models/v8/yolov8.yaml", verbose=True)
+    model = YOLO("ultralytics/cfg/models/v8/yolov8.yaml", verbose=True)  # 加载构建yaml自定义模型
+    # model = YOLO("yolov8n.pt", verbose=True)  # 加载构建预训练模型
+
+    # Train the model
+    results = model.train(data="coco8.yaml", epochs=100, imgsz=640) # 训练模型
