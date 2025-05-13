417e1aae50754e32c8592b399b9a361c952a3f3d
sywhs <2939068265@qq.com>
2025-05-13 16:39:24 +0800

    更新训练模型参数，增加训练轮数和批次大小；修改yolov8-CSN.yaml中的类别数量

--- test.py
+++ test.py
@@ -31,4 +31,6 @@ if __name__ == "__main__":
     # model = YOLO("yolov8n.pt", verbose=True)  # 加载构建预训练模型
 
     # Train the model
-    results = model.train(data="/home/nrc/MRE/my_yolov8/ultralytics/ultralytics/cfg/datasets/BDD100K.yaml", epochs=100, batch=32, imgsz=640, device=[0, 1, 2, 3]) # 训练模型
+    results = model.train(data="/home/nrc/MRE/myyolov8/ultralytics/ultralytics/cfg/datasets/BDD100K.yaml", epochs=300, batch=512, imgsz=640, device=[0, 1, 2, 3]) # 训练模型
+
+    # results = model.train(data="/home/nrc/MRE/my_yolov8/ultralytics/ultralytics/cfg/datasets/coco8.yaml", epochs=10, imgsz=640) # 训练模型
