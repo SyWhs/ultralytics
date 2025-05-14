92abcb9de5f401b8de0cf29ac4f5fcb65374779e
sywhs <2939068265@qq.com>
2025-05-14 22:55:05 +0800

    添加训练结果和预测图像文件，包括曲线图、混淆矩阵和标签图像

--- test.py
+++ test.py
@@ -34,6 +34,6 @@ if __name__ == "__main__":
     # model = YOLO("yolov8n.pt", verbose=True)  # 加载构建预训练模型
 
     # Train the model
-    # results = model.train(data="myyolov8/ultralytics/ultralytics/cfg/datasets/BDD100K.yaml", epochs=300, batch=512, imgsz=640, device=[0, 1, 2, 3]) # 训练模型
+    # results = model.train(data="myyolov8/ultralytics/ultralytics/cfg/datasets/BDD100K.yaml", epochs=200, batch=512, imgsz=640, device=[0, 1, 2, 3]) # 训练模型
 
     # results = model.train(data="/home/nrc/MRE/my_yolov8/ultralytics/ultralytics/cfg/datasets/coco8.yaml", epochs=10, imgsz=640) # 训练模型
