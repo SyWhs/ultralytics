4fbbf5491e4cdb1550177bad58d8d8d7930bb324
SyWhs <sywhs2020@nuaa.edu.cn>
2025-05-15 21:24:47 +0800

    更新模型加载逻辑，启用自定义模型配置并调整训练参数

--- test.py
+++ test.py
@@ -27,15 +27,18 @@ def demo():
 
 if __name__ == "__main__":
 
-    # model = YOLO("yolov8-CSN.yaml", verbose=True)  # 加载构建yaml自定义模型
+    model = YOLO("yolov8-CSN.yaml", verbose=True)  # 加载构建yaml自定义模型
 
     # model = YOLO("yolov8.yaml", verbose=True)  # 加载构建yaml自定义模型
 
-    model = YOLO("yolov8n.pt", verbose=True)  # 加载构建预训练模型
+    # model = YOLO("yolov8n.pt", verbose=True)  # 加载构建预训练模型
 
-    # model.load("yolov8n.pt")  # 加载预训练模型
+    model.load("ultralytics/runs/detect/train-yolov8-e200/weights/best.pt")  # 加载预训练模型
 
     # Train the model
-    # results = model.train(data="myyolov8/ultralytics/ultralytics/cfg/datasets/BDD100K.yaml", epochs=200, batch=512, imgsz=640, device=[0, 1, 2, 3]) # 训练模型
+    # results = model.train(data="./cfg/datasets/BDD100K.yaml", epochs=200, batch=512, imgsz=640, device=[0, 1, 2, 3]) # 训练模型
 
-    results = model.train(data="/home/nrc505/myyolov8/ultralytics/ultralytics/cfg/datasets/coco8.yaml", epochs=100, imgsz=640) # 训练模型
+    results = model.train(data="./cfg/datasets/BDD100K.yaml", 
+                          epochs=10, 
+                          batch=8,
+                          imgsz=640) # 训练模型
